7a5b69ccdf1feb398b7196d235f04f27081c13dc
mirko rettani <mirko6696@hotmail.it>
2021-02-07 18:24:05 +0100

    updateSquadra

--- Squadra.py
+++ Squadra.py
@@ -1,5 +1,6 @@
 from Enumerators import *
 from Partita import Partita 
+import math
 
 class Squadra:
     """Classe per la descrizione della singola squadra"""
@@ -96,3 +97,41 @@ class Squadra:
             return round((self.GetGoalSubitiInCasa() + self.GetGoalSubitiAway())/(self.GetGoalFattiInCasa() + self.GetGoalFattiAway() + self.GetGoalSubitiInCasa() + self.GetGoalSubitiAway()), 3)
         except ZeroDivisionError:
             return -1   
+
+    def GetValoreAttesoAttaccoHome(self) -> float:
+        goalFatti:int = self.GetGoalFattiInCasa()
+        partiteCasa:list[Partita] = self.GetPartiteWhereHome()
+        numeroPartite:int = len(partiteCasa)
+        mediaGoalFatti:float = goalFatti/numeroPartite
+        return mediaGoalFatti
+
+    def GetValoreAttesoAttaccoAway(self) -> float:
+        goalFatti:int = self.GetGoalFattiAway()
+        partiteAway:list[Partita] = self.GetPartiteWhereAway()
+        numeroPartite:int = len(partiteAway)
+        mediaGoalFatti:float = goalFatti/numeroPartite
+        return mediaGoalFatti
+
+    def GetValoreAttesoSubitiHome(self) -> float:
+        goalSubiti:int = self.GetGoalSubitiInCasa()
+        partiteCasa:list[Partita] = self.GetPartiteWhereHome()
+        numeroPartite:int = len(partiteCasa)
+        mediaGoalSubiti:float = goalSubiti/numeroPartite
+        return mediaGoalSubiti
+
+    def GetValoreAttesoSubitiAway(self) -> float:
+        goalSubiti:int = self.GetGoalSubitiAway()
+        partiteAway:list[Partita] = self.GetPartiteWhereAway()
+        numeroPartite:int = len(partiteAway)
+        mediaGoalSubiti:float = goalSubiti/numeroPartite
+        return mediaGoalSubiti
+
+    def GetPoissonValueHome(self, goalNumber:int)->float:
+        lamb:float = self.GetValoreAttesoAttaccoHome()
+        poisson:float = (lamb^goalNumber * (math.e^(-lamb)))/math.factorial(goalNumber)
+        return poisson
+
+    def GetPoissonValueHome(self, goalNumber:int)->float:
+        lamb:float = self.GetValoreAttesoAttaccoAway()
+        poisson:float = (lamb^goalNumber * (math.e^(-lamb)))/math.factorial(goalNumber)
+        return poisson
