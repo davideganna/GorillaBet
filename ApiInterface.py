6dada3d6b2532e37c8e6478978054000c57ffb21
mirko rettani <mirko6696@hotmail.it>
2021-02-11 20:25:32 +0100

    up

--- ApiInterface.py
+++ ApiInterface.py
@@ -3,17 +3,16 @@ import json
 
 class DashApi:
     """Classe middleware per l'API"""
-
+    headers = {
+            'x-rapidapi-host': "v3.football.api-sports.io",
+            'x-rapidapi-key': "6fb51c5c8788961e2f02bc09b221b3ce" #Chiave Mirko
+            #'x-rapidapi-key': "6fb51c5c8788961e2f02bc09b221b3ce" #Chiave Davide
+            #'x-rapidapi-key': "6fb51c5c8788961e2f02bc09b221b3ce" #Chiave Varsa
+            }
     @staticmethod
     def GetAllSquadre() -> list:    
-        headers = {
-        'x-rapidapi-host': "v3.football.api-sports.io",
-        'x-rapidapi-key': "6fb51c5c8788961e2f02bc09b221b3ce" #Chiave Mirko
-        #'x-rapidapi-key': "6fb51c5c8788961e2f02bc09b221b3ce" #Chiave Davide
-        #'x-rapidapi-key': "6fb51c5c8788961e2f02bc09b221b3ce" #Chiave Varsa
-        }
         conn = http.client.HTTPSConnection("v3.football.api-sports.io")
-        conn.request("GET", "/teams?league=135&season=2020", headers=headers)
+        conn.request("GET", "/teams?league=135&season=2020", headers=DashApi.headers)
         res = conn.getresponse()
         data = res.read()
         jsonResult = json.loads(data.decode("utf-8"))
