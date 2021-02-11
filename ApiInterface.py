0da5870fe56f3610704ad8ec67f3301b5d82a72c
mirko rettani <mirko6696@hotmail.it>
2021-02-11 20:23:42 +0100

    aaa

--- ApiInterface.py
+++ ApiInterface.py
@@ -9,6 +9,8 @@ class DashApi:
         headers = {
         'x-rapidapi-host': "v3.football.api-sports.io",
         'x-rapidapi-key': "6fb51c5c8788961e2f02bc09b221b3ce" #Chiave Mirko
+        #'x-rapidapi-key': "6fb51c5c8788961e2f02bc09b221b3ce" #Chiave Davide
+        #'x-rapidapi-key': "6fb51c5c8788961e2f02bc09b221b3ce" #Chiave Varsa
         }
         conn = http.client.HTTPSConnection("v3.football.api-sports.io")
         conn.request("GET", "/teams?league=135&season=2020", headers=headers)
