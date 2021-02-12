6b7a7e614a5d3c3cee8dcdff441d2ebf2d88b95b
Francesco <francesco.varsallona@gmail.com>
2021-02-12 17:47:16 +0100

    "Aggiunto chiave API Varsa

--- ApiInterface.py
+++ ApiInterface.py
@@ -1,40 +1,44 @@
-import http.client, urllib.parse
+import http.client
+import urllib.parse
 import json
+
 
 class DashApi:
     """Classe middleware per l'API"""
-    url:str = "v3.football.api-sports.io"
+    url: str = "v3.football.api-sports.io"
     headers = {
         'x-rapidapi-host': url,
-        'x-rapidapi-key': "6fb51c5c8788961e2f02bc09b221b3ce" #Chiave Mirko
-        #'x-rapidapi-key': "6fb51c5c8788961e2f02bc09b221b3ce" #Chiave Davide
-        #'x-rapidapi-key': "6fb51c5c8788961e2f02bc09b221b3ce" #Chiave Varsa
+        # 'x-rapidapi-key': "6fb51c5c8788961e2f02bc09b221b3ce"  # Chiave Mirko
+        # 'x-rapidapi-key': "6fb51c5c8788961e2f02bc09b221b3ce" #Chiave Davide
+        'x-rapidapi-key': "e5c09ffce045356e24a0c225e2352a4d"  # Chiave Varsa
     }
+
     @staticmethod
-    def GetAllSquadre() -> list:    
+    def GetAllSquadre() -> list:
         conn = http.client.HTTPSConnection("v3.football.api-sports.io")
-        conn.request("GET", "/teams?league=135&season=2020", headers=DashApi.headers)
+        conn.request("GET", "/teams?league=135&season=2020",
+                     headers=DashApi.headers)
         res = conn.getresponse()
         data = res.read()
         jsonResult = DashApi.GetJsonResponse(data)
-        squadre : list = []
+        squadre: list = []
         for t in jsonResult["response"]:
             squadre.append(t["team"])
             print(t["team"])
         return squadre
 
     @staticmethod
-    def GetFixtureAll() -> list:    
+    def GetFixtureAll() -> list:
         conn = http.client.HTTPSConnection(DashApi.url)
-        conn.request("GET", "/fixtures?next=10&league=135&season=2020", headers=DashApi.headers)
+        conn.request(
+            "GET", "/fixtures?next=10&league=135&season=2020", headers=DashApi.headers)
         res = conn.getresponse()
         data = res.read()
         jsonResult = DashApi.GetJsonResponse(data)
-        squadre : list = []
+        squadre: list = []
         print(jsonResult)
         return squadre
 
-
-    @staticmethod 
+    @staticmethod
     def GetJsonResponse(data):
         return json.loads(data.decode("utf-8"))
