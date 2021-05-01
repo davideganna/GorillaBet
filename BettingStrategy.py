29fba174a7ac6609e3a64fdbdd23eab0fbecf2b5
davideganna <52606991+davideganna@users.noreply.github.com>
2021-05-01 16:48:27 +0200

    Goals stats

--- BettingStrategy.py
+++ BettingStrategy.py
@@ -11,16 +11,36 @@ df = FileHelper.get_dataframe()
 df = df[["HomeTeam", "AwayTeam", "FTHG", "FTAG"]]
 df = df.rename(columns={"FTHG": "HomeGoals", "FTAG": "AwayGoals"})
 
-
 giornate = []
+home_goals_made = []  # Totale Goal fatti in casa
+away_goals_made = []  # Totale Goal fatti in trasferta
+home_goals_taken = [] # Totale Goal subiti in casa
+away_goals_taken = [] # Totale Goal subiti in trasferta
+
 for squadra in Squadre:
     giornate.append(
         df["HomeTeam"].value_counts()[squadra] + df["AwayTeam"].value_counts()[squadra]
     )
+    home_goals_made.append(
+        df.loc[df["HomeTeam"] == squadra]["HomeGoals"].sum()
+    )
+    away_goals_made.append(
+        df.loc[df["AwayTeam"] == squadra]["AwayGoals"].sum()
+    )
+    home_goals_taken.append(
+        df.loc[df["HomeTeam"] == squadra]["AwayGoals"].sum()
+    )
+    away_goals_taken.append(
+        df.loc[df["AwayTeam"] == squadra]["HomeGoals"].sum()
+    )
 
 d = {
     "Teams": Squadre,
-    "Giornate": giornate
+    "Giornate": giornate,
+    "GFc": home_goals_made,
+    "GFt": away_goals_made,
+    "GSc": home_goals_taken,
+    "GSt": away_goals_taken,
 }
 df = pd.DataFrame(data=d)
  
